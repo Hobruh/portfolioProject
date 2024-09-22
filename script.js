659af7f971cba84ae10ef6e09d21c8af30c9b08a
Adrian Gharineh <adrian.ghari@gmail.com>
2024-09-22 09:04:54 -0500

    Added some beginning JS to hide/show elements onscreen

---
+++ script.js
@@ -0,0 +1,16 @@
+const project1 = document.getElementById("project-1-img");
+const project2 = document.getElementById("project-2");
+const project3 = document.getElementById("project-3");
+const project1btn = document.getElementById("project-1-button");
+
+project1btn.addEventListener("click", () => {
+    if (project1.style.display === "none") {
+      project1.style.display = "block";
+      project2.style.display = "none";
+      project3.style.display = "none";
+    } else {
+      project1.style.display = "none";   
+      project2.style.display = "block";
+      project3.style.display = "block";  
+    }
+  });
