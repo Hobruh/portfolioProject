aca8ef7a02456b38f3fb61ad06dcec12e625c5f2
Adrian Gharineh <adrian.ghari@gmail.com>
2024-10-10 18:36:08 -0500

    fixed image margin for mobile

--- script.js
+++ script.js
@@ -6,6 +6,7 @@ const project1btn = document.getElementById("project-1-button");
 project1btn.addEventListener("click", () => {
     if (project1.style.display === "none") {
       project1.style.display = "block";
+      project1.style.marginLeft= "-100px";
       project2.style.display = "none";
       project3.style.display = "none";
     } else {
