ad555155e36e1965655291beb9c041a627abd7d8
Adrian Gharineh <adrian.ghari@gmail.com>
2024-10-27 11:06:26 -0500

    Added 2nd project correctly

--- script.js
+++ script.js
@@ -1,17 +1,22 @@
 const project1 = document.getElementById("project-1-img");
-const project2 = document.getElementById("project-2");
+const project2 = document.getElementById("project-2-img");
 const project3 = document.getElementById("project-3");
 const project1btn = document.getElementById("project-1-button");
+const project2btn = document.getElementById("project-2-button");
+
+// Initially, only project1 is visible
+project1.style.display = "none";
+project2.style.display = "none";
+project3.style.display = "none";
 
 project1btn.addEventListener("click", () => {
-    if (project1.style.display === "none") {
-      project1.style.display = "block";
-      project1.style.marginLeft= "-100px";
-      project2.style.display = "none";
-      project3.style.display = "none";
-    } else {
-      project1.style.display = "none";   
-      project2.style.display = "block";
-      project3.style.display = "block";  
-    }
-  });
+  project1.style.display = "block";
+  project2.style.display = "none";
+  project3.style.display = "none";
+});
+
+project2btn.addEventListener("click", () => {
+  project2.style.display = "block";
+  project1.style.display = "none";
+  project3.style.display = "none";
+});
